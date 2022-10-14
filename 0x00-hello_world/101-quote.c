c812bc434057a1bd1d02041134ed4d63a62aeae7
osaslaw <osagieizevbuwa@gmail.com>
2022-10-13 20:34:19 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-	      printf(
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	      printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 	return (1);
 }
