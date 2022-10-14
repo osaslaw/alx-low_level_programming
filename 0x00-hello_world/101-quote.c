1deb631f1d8c4dd11ca915d72704cc390d1bb343
osaslaw <osagieizevbuwa@gmail.com>
2022-10-13 20:40:24 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: Always 1 (Success<F5>)
  */
 int main(void)
 {
-	      printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-
-	return (1);
+	      printf("and that piece of art is useful
+			      \" - Dora Korpar, 2015-10-19\n", 59);
+	      return (1);
 }
